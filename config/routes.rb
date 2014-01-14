ab9b2173e950c5ae40d0f808d1db428b4e160873
Brian C. Brazil <bbrazil@gmail.com>
2014-01-13 23:02:34 -0500

    neatened up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 PicShare::Application.routes.draw do
   get "event_viewer/index"
   get "welcome/index"
-  resources :events
 
-  resources :photos
+  resources :events
+resources :photos
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
