d4049adf6a202f23d78ec9629d562ee9e6f80102
Brian C. Brazil <bbrazil@gmail.com>
2014-01-13 23:09:55 -0500

    added images list back in

--- app/controllers/event_viewer_controller.rb
+++ app/controllers/event_viewer_controller.rb
@@ -1,6 +1,6 @@
 class EventViewerController < ApplicationController
   def index
     @event = Event.find(params[:id])
-    #@images = @event.medias.map { |media| media.media_url }
+    @images = @event.medias.map { |media| media.media_url }
   end
 end
