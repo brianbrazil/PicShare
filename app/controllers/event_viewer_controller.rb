177ac41ad43af0a2e003423e747e516e55b21cc8
Brian C. Brazil <bbrazil@gmail.com>
2014-01-13 23:08:19 -0500

    put event back into controller

--- app/controllers/event_viewer_controller.rb
+++ app/controllers/event_viewer_controller.rb
@@ -1,7 +1,6 @@
 class EventViewerController < ApplicationController
   def index
-    puts "Got into index"
-    #@event = Event.find(params[:id])
+    @event = Event.find(params[:id])
     #@images = @event.medias.map { |media| media.media_url }
   end
 end
