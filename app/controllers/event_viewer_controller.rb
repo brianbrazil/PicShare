6dbf81240849717638a6d3ed0a0bafb1d3e5428b
Brian C. Brazil <bbrazil@gmail.com>
2014-01-13 22:46:33 -0500

    moved image shuffle to erb (where it belongs)

--- app/controllers/event_viewer_controller.rb
+++ app/controllers/event_viewer_controller.rb
@@ -2,6 +2,5 @@ class EventViewerController < ApplicationController
   def index
     @event = Event.find params[:id]
     @images = @event.medias.map { |media| media.media_url }
-    @images.shuffle!
   end
 end
