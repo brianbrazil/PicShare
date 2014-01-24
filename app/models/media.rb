b5c8debb65b49245221acf3ad6019e30ba239698
Brian C. Brazil <bbrazil@gmail.com>
2014-01-23 19:40:21 -0500

    using environment variable to store AWS ID & secret

--- app/models/media.rb
+++ app/models/media.rb
@@ -7,14 +7,15 @@ class Media
   field :adult_content, type: Mongoid::Boolean
 
   def media_file=(file)
+    p ENV['PICSHARE_AWS_ID']
     self._s3_filename = SecureRandom.uuid
-    s3 = AWS::S3.new(access_key_id: 'AKIAJ36DKF7OEHM5WBSA', secret_access_key: ENV['AWS_SECRET'])
+    s3 = AWS::S3.new(access_key_id: ENV['PICSHARE_AWS_ID'], secret_access_key: ENV['PICSHARE_AWS_SECRET'])
     obj = AWS::S3::S3Object.new(s3.buckets['PicShare'], self._s3_filename)
     obj.write(file)
   end
 
   def media_url
-    s3 = AWS::S3.new(access_key_id: 'AKIAJ36DKF7OEHM5WBSA', secret_access_key: ENV['AWS_SECRET'])
+    s3 = AWS::S3.new(access_key_id: ENV['PICSHARE_AWS_ID'], secret_access_key: ENV['PICSHARE_AWS_SECRET'])
     obj = s3.buckets['PicShare'].objects[self._s3_filename]
     obj.url_for(:read)
   end
@@ -22,7 +23,7 @@ class Media
   field :_s3_filename, type: String
 
   def delete(options)
-    s3 = AWS::S3.new(access_key_id: 'AKIAJ36DKF7OEHM5WBSA', secret_access_key: ENV['AWS_SECRET'])
+    s3 = AWS::S3.new(access_key_id: ENV['PICSHARE_AWS_ID'], secret_access_key: ENV['PICSHARE_AWS_SECRET'])
     s3.buckets['PicShare'].objects[self._s3_filename].delete
     super
   end
