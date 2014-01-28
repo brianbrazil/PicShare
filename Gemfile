bfdb308ea2bf54880ba005b41a9f7decad080a59
Brian C. Brazil <bbrazil@gmail.com>
2014-01-28 18:50:39 -0500

    removed google analytics

--- Gemfile
+++ Gemfile
@@ -23,7 +23,6 @@ end
 
 group :production do
   gem 'rails_12factor'
-  gem 'google-analytics-rails'
 end
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 PicShare::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  GA.tracker = 'UA-47463690-1'
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
