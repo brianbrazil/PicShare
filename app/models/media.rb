e1a8d9de9e3c524a7173c807a4b481b32fd75770
Brian C. Brazil <bbrazil@gmail.com>
2014-02-07 16:21:06 -0500

    refactored and cleaned up a bit

--- app/models/media.rb
+++ app/models/media.rb
@@ -9,25 +9,22 @@ class Media
 
   def media_file=(file)
     self._s3_filename = SecureRandom.uuid
-    obj = AWS::S3::S3Object.new(s3.buckets['PicShare'], self._s3_filename)
+    obj = AWS::S3::S3Object.new(bucket, self._s3_filename)
     obj.write(file)
   end
 
   def media_url
-    obj = s3.buckets['PicShare'].objects[self._s3_filename]
+    obj = bucket.objects[self._s3_filename]
     obj.url_for(:read)
   end
 
   def media_url_240_wide
-    resized_s3_filename = "#{self._s3_filename}-240-wide"
-    if s3.buckets['PicShare-resized'].objects[resized_s3_filename].exists?
-      p 'return from cache'
-      s3.buckets['PicShare-resized'].objects[resized_s3_filename].url_for(:read)
+    if resized_bucket.objects[resized_s3_filename].exists?
+      resized_bucket.objects[resized_s3_filename].url_for(:read)
     else
-      p 'generating new'
-      obj = s3.buckets['PicShare'].objects[self._s3_filename]
+      obj = bucket.objects[self._s3_filename]
       Image.from_blob(obj.read).first.change_geometry('240x') do |cols, rows, img|
-        resized_obj = AWS::S3::S3Object.new(s3.buckets['PicShare-resized'], resized_s3_filename)
+        resized_obj = AWS::S3::S3Object.new(resized_bucket, resized_s3_filename)
         resized_obj.write(img.resize(cols, rows).to_blob)
         resized_obj.url_for(:read)
       end
@@ -35,13 +32,25 @@ class Media
   end
 
   def delete(options)
-    s3.buckets['PicShare'].objects[self._s3_filename].delete
+    bucket.objects[self._s3_filename].delete
     super
   end
 
   private
 
   field :_s3_filename, type: String
+
+  def resized_s3_filename
+    "#{self._s3_filename}-240-wide"
+  end
+
+  def bucket
+    s3.buckets['PicShare']
+  end
+
+  def resized_bucket
+    s3.buckets['PicShare-resized']
+  end
 
   def s3
     AWS::S3.new(access_key_id: ENV['PICSHARE_AWS_ID'], secret_access_key: ENV['PICSHARE_AWS_SECRET'])
