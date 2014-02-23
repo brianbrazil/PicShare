afdbae1e5e0e82134eec23dfe835a4a14e30b44c
Brian Brazil <brian.brazil@teespring.com>
2014-02-23 11:53:44 -0500

    extracted saving as 240

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,18 +3,23 @@ class Photo < Media
 
   def media_file=(file)
     super
+    save_as_240
   end
 
   def media_url_240_wide
     if resized_bucket.objects[resized_s3_filename].exists?
       resized_bucket.objects[resized_s3_filename].url_for(:read)
     else
-      obj = bucket.objects[self._s3_filename]
-      Image.from_blob(obj.read).first.change_geometry('240x') do |cols, rows, img|
-        resized_obj = AWS::S3::S3Object.new(resized_bucket, resized_s3_filename)
-        resized_obj.write(img.resize(cols, rows).to_blob)
-        resized_obj.url_for(:read)
-      end
+      save_as_240
+    end
+  end
+
+  def save_as_240
+    obj = bucket.objects[self._s3_filename]
+    Image.from_blob(obj.read).first.change_geometry('240x') do |cols, rows, img|
+      resized_obj = AWS::S3::S3Object.new(resized_bucket, resized_s3_filename)
+      resized_obj.write(img.resize(cols, rows).to_blob)
+      resized_obj.url_for(:read)
     end
   end
 
